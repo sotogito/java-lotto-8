a452ba11fec5d4e799584168fa8e05a3b0caad34
sotogito <sotogitoarchive@gmail.com>
2025-10-29 21:43:43 +0900

    feat : Lotto 유효검증 추가

---
+++ src/main/java/lotto/domain/model/Lotto.java
@@ -0,0 +1,42 @@
+package lotto.domain.model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateNumberSize(numbers);
+        validateNumbers(numbers);
+        validateDuplicationNumbers(numbers);
+
+        this.numbers = numbers;
+    }
+
+    private void validateNumberSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateNumbers(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number == null) {
+                throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+            }
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("로또 번호 범위는 1~45까지 입니다.");
+            }
+        }
+    }
+
+    private void validateDuplicationNumbers(List<Integer> numbers) {
+        Set<Integer> noDuplication = new HashSet<>(numbers);
+        if (noDuplication.size() != numbers.size()) {
+            throw new IllegalArgumentException("로또 번호를 허용하지 않습니다.");
+        }
+    }
+
+}
