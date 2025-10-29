9c42f27677794b7cb0e4f8872d6548c915689127
sotogito <sotogitoarchive@gmail.com>
2025-10-30 00:19:33 +0900

    feat : 재입력 예외 출력 클래스 생성

---
+++ src/main/java/lotto/ui/ExceptionHandler.java
@@ -0,0 +1,12 @@
+package lotto.ui;
+
+import lotto.common.exception.RereadRequestException;
+
+public class ExceptionHandler {
+    private final static String EXCEPTION_FORMAT = "[ERROR] %s\n";
+
+    public static void read(RereadRequestException exception) {
+        System.out.printf(EXCEPTION_FORMAT, exception.getMessage());
+    }
+
+}
