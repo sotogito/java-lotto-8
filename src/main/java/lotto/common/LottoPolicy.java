a7b735203a0027420ceb821091615bdbf9a49e7e
sotogito <sotogitoarchive@gmail.com>
2025-10-29 22:33:30 +0900

    feat : LottoPolicy 상수 생성

---
+++ src/main/java/lotto/common/LottoPolicy.java
@@ -0,0 +1,13 @@
+package lotto.common;
+
+public class LottoPolicy {
+    public final static int LOTTO_PRICE = 1_000;
+    public final static int LOTTO_MIN_PURCHASE_PRICE = 1_000;
+    public final static int LOTTO_MAX_PURCHASE_PRICE = 100_000;
+
+    public final static int LOTTO_MIN_NUMBER = 1;
+    public final static int LOTTO_MAX_NUMBER = 45;
+
+    public final static int MAIN_LOTTO_NUMBER_COUNT = 6;
+
+}
