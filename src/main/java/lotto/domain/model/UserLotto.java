49ac180302d61c84c94a010637b633be75286d60
sotogito <sotogitoarchive@gmail.com>
2025-10-30 02:17:55 +0900

    fix : 보너스 로또 숫자 범위 유효검증 추가

--- src/main/java/lotto/domain/model/UserLotto.java
+++ src/main/java/lotto/domain/model/UserLotto.java
@@ -1,12 +1,13 @@
 package lotto.domain.model;
 
+import lotto.common.constants.LottoPolicy;
 import lotto.common.exception.RereadRequestException;
 
 public class UserLotto {
     private final Lotto mainLotto;
     private final int bonusNumber;
 
-    public UserLotto(Lotto mainLotto, Integer bonusNumber) {
+    private UserLotto(Lotto mainLotto, Integer bonusNumber) {
         this.mainLotto = mainLotto;
         this.bonusNumber = bonusNumber;
     }
@@ -17,6 +18,10 @@ public class UserLotto {
         }
         if (mainLotto.isContained(bonusNumber)) {
             throw new RereadRequestException("보너스 번호와 6자리 로또는 중복될 수 없습니다.");
+        }
+        if (bonusNumber < LottoPolicy.LOTTO_MIN_NUMBER
+                || bonusNumber > LottoPolicy.LOTTO_MAX_NUMBER) {
+            throw new RereadRequestException("로또 번호 범위는 1~45까지 입니다.");
         }
         return new UserLotto(mainLotto, bonusNumber);
     }
