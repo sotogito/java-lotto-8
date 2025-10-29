8cd286b9e89ef7d5860f9f1b650155818ec20fff
sotogito <sotogitoarchive@gmail.com>
2025-10-29 22:10:28 +0900

    feat : UserLotto 보너스번호 유효검증과 UserLotto 반환 static 메서드 추가

---
+++ src/main/java/lotto/domain/model/UserLotto.java
@@ -0,0 +1,22 @@
+package lotto.domain.model;
+
+public class UserLotto {
+    private final Lotto mainLotto;
+    private final int bonusNumber;
+
+    public UserLotto(Lotto mainLotto, Integer bonusNumber) {
+        this.mainLotto = mainLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static UserLotto createUserLotto(Lotto mainLotto, Integer bonusNumber) {
+        if (bonusNumber == null) {
+            throw new IllegalArgumentException("보너스 번호를 입력해주세요.");
+        }
+        if (mainLotto.isContained(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호와 6자리 로또는 중복될 수 없습니다.");
+        }
+        return new UserLotto(mainLotto, bonusNumber);
+    }
+
+}
