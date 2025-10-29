40af40cda19cb540af8a6e4a6ff2255720b16fce
sotogito <sotogitoarchive@gmail.com>
2025-10-29 22:38:38 +0900

    refactor : UserLotto 내부 팩토리 메서드명 변경 적용

--- src/test/java/lotto/domain/model/UserLottoTest.java
+++ src/test/java/lotto/domain/model/UserLottoTest.java
@@ -11,7 +11,7 @@ class UserLottoTest {
     void 보너스_번호_6자리_로또와_중복일_경우_예외처리() {
         Lotto mainLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
-        assertThatThrownBy(() -> UserLotto.createUserLotto(mainLotto, 1))
+        assertThatThrownBy(() -> UserLotto.create(mainLotto, 1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
