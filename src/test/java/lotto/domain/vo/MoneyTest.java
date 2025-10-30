9205ac909cf2e7a920b4964fb7fe24ec79d38f4c
sotogito <sotogitoarchive@gmail.com>
2025-10-30 21:19:25 +0900

    test : Money 수익률 계산 테스트

--- src/test/java/lotto/domain/vo/MoneyTest.java
+++ src/test/java/lotto/domain/vo/MoneyTest.java
@@ -1,11 +1,24 @@
 package lotto.domain.vo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MoneyTest {
+
+    @Test
+    void 수익률_계산() {
+        Money money = new Money(5000);
+        long totalPrice = 5000L;
+
+        BigDecimal actual = money.calculateYield(totalPrice);
+        BigDecimal expected = BigDecimal.valueOf(100.0);
+
+        assertThat(actual).isEqualTo(expected);
+    }
 
     @DisplayName("구매 금액은 1,000단위로 입력할 수 있다.")
     @Test
