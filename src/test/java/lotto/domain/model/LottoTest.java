b5eefacbb26b59b9387450a0c18a6195a01a015b
sotogito <sotogitoarchive@gmail.com>
2025-10-30 16:53:31 +0900

    test : 두 로또의 일치하는 숫자 개수 반환 테스트

--- src/test/java/lotto/domain/model/LottoTest.java
+++ src/test/java/lotto/domain/model/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto.domain.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -7,6 +8,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
+
+    @Test
+    void 두_로또의_일치하는_갯수_반환() {
+        Lotto lottoA = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lottoB = new Lotto(List.of(5, 6, 7, 8, 9, 10));
+
+        int actual = lottoA.getMatchingCount(lottoB);
+        int expected = 2;
+
+        assertThat(actual).isEqualTo(expected);
+    }
 
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
