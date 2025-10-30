1fc11965dad5dc5e963b2b3991ada585f6050632
sotogito <sotogitoarchive@gmail.com>
2025-10-30 17:09:02 +0900

    test : UserLotto 보너스 숫자 일치 여부 반환 테스트

--- src/test/java/lotto/domain/model/UserLottoTest.java
+++ src/test/java/lotto/domain/model/UserLottoTest.java
@@ -1,11 +1,25 @@
 package lotto.domain.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class UserLottoTest {
+
+    @Test
+    void 보너스번호_일치_여부() {
+        UserLotto userLotto = UserLotto.create(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                7
+        );
+        Lotto purchasedLotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+
+        boolean actual = userLotto.hasBonusNumberFrom(purchasedLotto);
+
+        assertThat(actual).isTrue();
+    }
 
     @Test
     void 보너스_번호_6자리_로또와_중복일_경우_예외처리() {
