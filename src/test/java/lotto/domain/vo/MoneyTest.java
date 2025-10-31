f4bdc98574aee4b403b7ff6a477b84cc39c44ed3
sotogito <sotogitoarchive@gmail.com>
2025-10-31 19:24:11 +0900

    test : Money 구매 로또 수량 계산 테스트

--- src/test/java/lotto/domain/vo/MoneyTest.java
+++ src/test/java/lotto/domain/vo/MoneyTest.java
@@ -10,6 +10,16 @@ import org.junit.jupiter.api.Test;
 class MoneyTest {
 
     @Test
+    void 구매_로또_수량_계상() {
+        Money money = new Money(1000);
+
+        int actual = money.calculatePurchasedQuantity();
+        int expected = 1;
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
     void 수익률_계산() {
         Money money = new Money(5000);
         long totalPrice = 5000L;
@@ -27,7 +37,7 @@ class MoneyTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또는 1000장까지 구매할 수 있다..")
+    @DisplayName("로또는 1000장까지 구매할 수 있다.")
     @Test
     void 구매_로또_수량_예외처리() {
         assertThatThrownBy(() -> new Money(10000000))
