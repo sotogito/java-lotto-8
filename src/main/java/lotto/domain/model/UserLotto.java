1f708f70dcbef1b0f1dec0387d3e05116b8e8f3b
sotogito <sotogitoarchive@gmail.com>
2025-10-31 20:29:57 +0900

    refactor : UserLotto 유효검증 메서드 리팩토링

--- src/main/java/lotto/domain/model/UserLotto.java
+++ src/main/java/lotto/domain/model/UserLotto.java
@@ -12,6 +12,14 @@ public class UserLotto {
         this.bonusNumber = bonusNumber;
     }
 
+    public static UserLotto create(Lotto mainLotto, Integer bonusNumber) {
+        validateNotNull(mainLotto, bonusNumber);
+        validateBonusNumberRange(bonusNumber);
+        validateBonusNumberDuplicateWithMainLotto(mainLotto, bonusNumber);
+
+        return new UserLotto(mainLotto, bonusNumber);
+    }
+
     public int matchMainLotto(Lotto purchasedLotto) {
         return mainLotto.getMatchingCount(purchasedLotto);
     }
@@ -20,18 +28,26 @@ public class UserLotto {
         return purchasedLotto.isContained(bonusNumber);
     }
 
-    public static UserLotto create(Lotto mainLotto, Integer bonusNumber) {
+    private static void validateNotNull(Lotto mainLotto, Integer bonusNumber) {
+        if (mainLotto == null) {
+            throw new IllegalArgumentException("예기치 못한 오류가 발생했습니다.");
+        }
         if (bonusNumber == null) {
             throw new RereadRequestException("보너스 번호를 입력해주세요.");
         }
-        if (mainLotto.isContained(bonusNumber)) {
-            throw new RereadRequestException("보너스 번호와 6자리 로또는 중복될 수 없습니다.");
-        }
+    }
+
+    private static void validateBonusNumberRange(Integer bonusNumber) {
         if (bonusNumber < LottoPolicy.LOTTO_MIN_NUMBER
                 || bonusNumber > LottoPolicy.LOTTO_MAX_NUMBER) {
             throw new RereadRequestException("로또 번호 범위는 1~45까지 입니다.");
         }
-        return new UserLotto(mainLotto, bonusNumber);
+    }
+
+    private static void validateBonusNumberDuplicateWithMainLotto(Lotto mainLotto, Integer bonusNumber) {
+        if (mainLotto.isContained(bonusNumber)) {
+            throw new RereadRequestException("보너스 번호와 6자리 로또는 중복될 수 없습니다.");
+        }
     }
 
 }
