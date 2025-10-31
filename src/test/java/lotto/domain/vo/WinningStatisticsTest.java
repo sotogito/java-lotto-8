855fd9dd7d1b00c814c2e5fe9980c41ec50f447d
sotogito <sotogitoarchive@gmail.com>
2025-10-31 19:34:41 +0900

    test : WinningStatistics 모든 행킹과 매칭된 랭킹 결과 반환 테스트

--- src/test/java/lotto/domain/vo/WinningStatisticsTest.java
+++ src/test/java/lotto/domain/vo/WinningStatisticsTest.java
@@ -4,9 +4,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
 import lotto.common.constants.Rank;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningStatisticsTest {
+
+    @DisplayName("매칭된 Rank와 모든 Rank를 반환한다. 단, NOTHING은 반환하지 않는다.")
+    @Test
+    void 모든_랭킹과_매칭_랭킹_결과_반환() {
+        WinningStatistics winningStatistics = new WinningStatistics(
+                Map.of(
+                        Rank.FIFTH, 2,
+                        Rank.NOTHING, 5
+                )
+        );
+
+        Map<Rank, Integer> actual = winningStatistics.getWinningStatisticsWithAllRank();
+
+        assertThat(actual)
+                .containsEntry(Rank.FIFTH, 2)
+                .containsEntry(Rank.FOURTH, 0)
+                .containsEntry(Rank.THIRD, 0)
+                .containsEntry(Rank.SECOND, 0)
+                .containsEntry(Rank.FIRST, 0)
+                .doesNotContainEntry(Rank.NOTHING, 5);
+    }
 
     @Test
     void 최종_상금_계산() {
