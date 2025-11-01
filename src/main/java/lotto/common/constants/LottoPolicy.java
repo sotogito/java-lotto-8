c0d616ace1dfc4852f0da5e2489a8a102dfebd5b
sotogito <sotogitoarchive@gmail.com>
2025-11-01 22:59:47 +0900

    chore : LottoPolicy 상수 순서 변경

--- src/main/java/lotto/common/constants/LottoPolicy.java
+++ src/main/java/lotto/common/constants/LottoPolicy.java
@@ -1,13 +1,12 @@
 package lotto.common.constants;
 
 public class LottoPolicy {
+    public final static int MIN_NUMBER = 1;
+    public final static int MAX_NUMBER = 45;
+    public final static int MAIN_LOTTO_NUMBER_COUNT = 6;
+
     public final static int PRICE = 1_000;
     public final static int MIN_PURCHASE_PRICE = 1_000;
     public final static int MAX_PURCHASE_PRICE = 100_000;
-
-    public final static int MIN_NUMBER = 1;
-    public final static int MAX_NUMBER = 45;
-
-    public final static int MAIN_LOTTO_NUMBER_COUNT = 6;
 
 }
