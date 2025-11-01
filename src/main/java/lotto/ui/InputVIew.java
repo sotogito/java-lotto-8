d8733ee81a835de9783b8f6e5215269402e73d07
sotogito <sotogitoarchive@gmail.com>
2025-11-02 03:30:12 +0900

    refactor : InputView Reader, NumberParser 적용

--- src/main/java/lotto/ui/InputVIew.java
+++ src/main/java/lotto/ui/InputVIew.java
@@ -1,11 +1,7 @@
 package lotto.ui;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import lotto.common.exception.LottoError;
-import lotto.common.exception.RereadRequestException;
 
 public class InputVIew {
     private final static String READ_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
@@ -13,62 +9,26 @@ public class InputVIew {
     private final static String READ_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public static int readPurchaseAmount() {
-        try {
-            System.out.println(READ_PURCHASE_AMOUNT);
+        System.out.println(READ_PURCHASE_AMOUNT);
 
-            return Integer.parseInt(read());
-        } catch (NumberFormatException e) {
-            throw new RereadRequestException(LottoError.INPUT_NOT_NUMBER);
-        }
+        return NumberParser.parseNumber(Reader.read());
     }
 
     public static List<Integer> readMainLottoNumbers() {
-        try {
-            System.out.println(READ_MAIN_LOTTO_NUMBERS);
+        System.out.println(READ_MAIN_LOTTO_NUMBERS);
 
-            return parseNumbers(read());
-        } catch (NumberFormatException e) {
-            throw new RereadRequestException(LottoError.INPUT_NOT_NUMBER);
-        }
+        return NumberParser.parseNumbers(Reader.read());
     }
 
     public static int readBonusNumber() {
-        try {
-            System.out.println();
-            System.out.println(READ_BONUS_NUMBER);
+        System.out.println();
+        System.out.println(READ_BONUS_NUMBER);
 
-            return Integer.parseInt(read());
-        } catch (NumberFormatException e) {
-            throw new RereadRequestException(LottoError.INPUT_NOT_NUMBER);
-        }
+        return NumberParser.parseNumber(Reader.read());
     }
 
     public static void consoleClose() {
         Console.close();
-    }
-
-    private static String read() {
-        try {
-            String input = Console.readLine().trim();
-            if (input.isEmpty()) {
-                throw new RereadRequestException(LottoError.INVALID_INPUT);
-            }
-            return input;
-        } catch (NullPointerException e) {
-            throw new RereadRequestException(LottoError.INVALID_INPUT);
-        }
-    }
-
-    private static List<Integer> parseNumbers(String inputNumbers) {
-        List<Integer> lottoNumbers = new ArrayList<>();
-
-        String[] splitNumbers = inputNumbers.split(",");
-        for (String splitNumber : splitNumbers) {
-            String number = splitNumber.trim();
-
-            lottoNumbers.add(Integer.parseInt(number));
-        }
-        return Collections.unmodifiableList(lottoNumbers);
     }
 
 }
