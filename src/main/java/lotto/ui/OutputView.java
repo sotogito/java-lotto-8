05d8bafcc87b4cb10fc52e8189f78a78af75c51d
sotogito <sotogitoarchive@gmail.com>
2025-11-02 04:10:21 +0900

    refactor : OutputView 리팩토링

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -25,27 +25,29 @@ public class OutputView {
     public static void writeWinningStatistics(WinningStatistics winningStatistics) {
         StringJoiner result = new StringJoiner("\n");
 
-        Map<Rank, Integer> allRank = winningStatistics.getWinningStatisticsWithAllRank();
-        for (Map.Entry<Rank, Integer> entry : allRank.entrySet()) {
+        for (Map.Entry<Rank, Integer> entry :
+                winningStatistics.getWinningStatisticsWithAllRank().entrySet()) {
             Rank rank = entry.getKey();
             Integer count = entry.getValue();
 
-            if (rank.equals(Rank.SECOND)) {
-                result.add(String.format(WRITE_MATCHING_RANK_WITH_BONUS_FORMAT,
-                        rank.getMatchCount(), rank.getPrizeMoney(), count));
-                continue;
-            }
-            result.add(String.format(WRITE_MATCHING_RANK_FORMAT,
-                    rank.getMatchCount(), rank.getPrizeMoney(), count));
+            result.add(getRankPrintout(rank, count));
         }
-
         System.out.println();
         System.out.print(WRITE_WINNING_STATISTICS_HEAD);
-        System.out.println(result.toString());
+        System.out.println(result);
     }
 
     public static void writeYield(BigDecimal yield) {
         System.out.printf(WRITE_YIELD_FORMAT, yield.doubleValue());
+    }
+
+    private static String getRankPrintout(Rank rank, Integer count) {
+        if (rank.equals(Rank.SECOND)) {
+            return String.format(WRITE_MATCHING_RANK_WITH_BONUS_FORMAT,
+                    rank.getMatchCount(), rank.getPrizeMoney(), count);
+        }
+        return String.format(WRITE_MATCHING_RANK_FORMAT,
+                rank.getMatchCount(), rank.getPrizeMoney(), count);
     }
 
 }
